8e74f9e9b27359b07e0e8c553e0aab5ad82e1b0e
Felipe Barraza <felipe.barraza.vega@gmail.com>
2024-10-23 00:55:54 -0300

    Fixc errors

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,5 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Typography, Row, Col, Card, Button, Tag, Table, Descriptions} from "antd";
+import {
+  Typography,
+  Row,
+  Col,
+  Card,
+  Button,
+  Tag,
+  Table,
+  Descriptions,
+} from "antd";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import {
   ClockCircleFilled,
@@ -26,12 +35,12 @@ const Dashboard = () => {
   const [typeMeditions, setTypeMeditions] = useState([]);
 
   const Ranking = (data) => {
-    var order = state.user.profile.groups.sort((a, b) => b.points - a.points);
+    var order = state.user.groups.sort((a, b) => b.points - a.points);
     const rankedData = order.map((item, index) => {
       return { ...item, ranking: index + 1 };
     });
     var myRanking =
-      state.user.profile.groups
+      state.user.groups
         .sort((a, b) => b.points - a.points)
         .findIndex((item) => item.id === state.user.team.id) + 1;
 
@@ -41,76 +50,89 @@ const Dashboard = () => {
   const filterActivities = (activities) => {
     return activities
       .filter((item) => item.activity) // Filtrar solo los elementos que tienen una propiedad 'activity'
-      
+
       .map((item) => {
         return {
           name: item.activity.name,
-          value_activity: parseFloat(item.activity.value),
+          value_activity: parseFloat(item.activity.value) || 0,
           type_stats: item.activity.type_stats,
-          value_team:  item.value, // Agregar la propiedad 'value_team' con valor inicial 0
+          value_team: item.value, // Agregar la propiedad 'value_team' con valor inicial 0
         };
       }); // Crear un nuevo array con solo 'activity.name', 'value', y 'value_team'
   };
 
- 
-const filterActivitiesUser = (activities) => {
-  
+  const filterActivitiesUser = (activities) => {
+    const filteredActivities = activities.filter((item) => item.activity); // Filtrar solo los elementos que tienen una propiedad 'activity'
 
-  const filteredActivities = activities.filter((item) => item.activity); // Filtrar solo los elementos que tienen una propiedad 'activity'
+    const allTimeActivities = filteredActivities;
 
-  const allTimeActivities = filteredActivities;
+    const allTimeWeeks = {};
+    allTimeActivities.forEach((item) => {
+      const activityDate = new Date(item.start_date_time);
+      const activityWeek = getWeekNumber(activityDate);
+      if (!allTimeWeeks[activityWeek]) {
+        allTimeWeeks[activityWeek] = {
+          totalActivities: 0,
+          totalCompletedActivities: 0,
+          totalDuration: 0,
+          totalCaloriesBurned: 0,
+        };
+      }
+      allTimeWeeks[activityWeek].totalActivities++;
+      if (item.is_completed) {
+        allTimeWeeks[activityWeek].totalCompletedActivities++;
+        allTimeWeeks[activityWeek].totalCaloriesBurned +=
+          item.activity.calories_burned;
+      }
+      allTimeWeeks[activityWeek].totalDuration += item.duration;
+    });
 
-  const allTimeWeeks = {};
-  allTimeActivities.forEach((item) => {
-    const activityDate = new Date(item.start_date_time);
-    const activityWeek = getWeekNumber(activityDate);
-    if (!allTimeWeeks[activityWeek]) {
-      allTimeWeeks[activityWeek] = {
-        totalActivities: 0,
-        totalCompletedActivities: 0,
-        totalDuration: 0,
-        totalCaloriesBurned: 0,
+    const dataSource = Object.entries(allTimeWeeks).map(
+      (
+        [
+          week,
+          {
+            totalActivities,
+            totalCompletedActivities,
+            totalDuration,
+            totalCaloriesBurned,
+          },
+        ],
+        index
+      ) => ({
+        week: `Week ${index + 1}`,
+        totalActivities,
+        totalCompletedActivities,
+        totalDuration,
+        totalCaloriesBurned,
+      })
+    );
+
+    var type_meditions = state.user.profile.type_meditions;
+
+    const dataSourceWithMeditions = dataSource.map((data) => {
+      return {
+        ...data,
       };
-    }
-    allTimeWeeks[activityWeek].totalActivities++;
-    if (item.is_completed) {
-      allTimeWeeks[activityWeek].totalCompletedActivities++;
-      allTimeWeeks[activityWeek].totalCaloriesBurned += item.activity.calories_burned;
-    }
-    allTimeWeeks[activityWeek].totalDuration += item.duration;
-  });
+    });
 
-  const dataSource = Object.entries(allTimeWeeks).map(([week, { totalActivities, totalCompletedActivities, totalDuration, totalCaloriesBurned }], index) => ({
-    week: `Week ${index + 1}`,
-    totalActivities,
-    totalCompletedActivities,
-    totalDuration,
-    totalCaloriesBurned,
-  }));
+    return dataSourceWithMeditions.reverse();
+  };
 
-  var type_meditions = state.user.profile.type_meditions;
-
-  const dataSourceWithMeditions = dataSource.map((data) => {
-    return {
-      ...data,
-    };
-  });
-
-  return dataSourceWithMeditions.reverse();
-};
-
-function getWeekNumber(date) {
-  const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
-  const pastDaysOfYear = (date - firstDayOfYear) / 86400000;
-  return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
-}
+  function getWeekNumber(date) {
+    const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+    const pastDaysOfYear = (date - firstDayOfYear) / 86400000;
+    return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+  }
 
   useEffect(() => {
     const filteredActivities = filterActivities(
       state.user.profile.total_activities_group
     );
 
-    const filteredActivitiesUser = filterActivitiesUser(state.user.profile.total_activities_user);
+    const filteredActivitiesUser = filterActivitiesUser(
+      state.user.profile.total_activities_user
+    );
 
     dispatch({
       type: "SET_ACTIVITIES",
@@ -249,7 +271,7 @@ function getWeekNumber(date) {
                 <Ranking />/{" "}
                 <span style={{ fontSize: "22px" }}>
                   {" "}
-                  {state.user.profile.groups.length} Equipos{" "}
+                  {state.user.groups.length} Equipos{" "}
                 </span>
               </Title>
             </Col>
@@ -280,27 +302,28 @@ function getWeekNumber(date) {
         </Row>
       </Col>
       <Col span={24}>
-      {state.dashboard && (
-        <Row style={{marginBottom:"20px", }} justify={"space-around"}>
-          {state.dashboard.activities.map((item) => (
-            <Col style={{ width: 150, height: 140 }}>
-              <CircularProgressbar
-                value={item.value_team}
-                maxValue={item.value_activity}
-                text={
-                  item.type_stats === "INCREMENTAL"
-                    ? `${item.value_team}/${item.value_activity}`
-                    : `${(item.value_team * 100)/item.value_activity}%`
-                }
-                styles={styles.circlePro}
-              />
-              <Title level={4} style={{ textAlign: "center" }}>
-                {item.name}
-              </Title>
-            </Col>
-          ))}
-        </Row>
-      )}</Col>
+        {state.dashboard && (
+          <Row style={{ marginBottom: "20px" }} justify={"space-around"}>
+            {state.dashboard.activities.map((item) => (
+              <Col style={{ width: 150, height: 140 }}>
+                <CircularProgressbar
+                  value={item.value_team}
+                  maxValue={item.value_activity}
+                  text={
+                    item.type_stats === "INCREMENTAL"
+                      ? `${item.value_team}/${item.value_activity}`
+                      : `${(item.value_team * 100) / item.value_activity}%`
+                  }
+                  styles={styles.circlePro}
+                />
+                <Title level={4} style={{ textAlign: "center" }}>
+                  {item.name}
+                </Title>
+              </Col>
+            ))}
+          </Row>
+        )}
+      </Col>
       <Col span={24} style={{ marginTop: "20px" }}>
         <Row
           align="middle"
@@ -346,38 +369,80 @@ function getWeekNumber(date) {
         </Row>
       </Col>
       <Col span={24}>
-        <Row justify={"space-between"}style={{ marginBottom: "20px" }}>
-          <Col xl={11} xs={24} >
-          {stats_personal.length>0 && 
-          <Descriptions title="Semana Actual" bordered>
-            <Descriptions.Item label="Pruebas en competencia " span={3}>{stats_personal[0].totalActivities}</Descriptions.Item>
-            <Descriptions.Item label="Pruebas completadas" span={3}>{stats_personal[0].totalCompletedActivities}</Descriptions.Item>
-            <Descriptions.Item label="Minutos ejercitados" span={3}>{stats_personal[0].totalDuration}</Descriptions.Item>
-            <Descriptions.Item label="Kcal quemadas" span={3}>{stats_personal[0].totalCaloriesBurned}</Descriptions.Item>
-          </Descriptions>}
+        <Row justify={"space-between"} style={{ marginBottom: "20px" }}>
+          <Col xl={11} xs={24}>
+            {stats_personal.length > 0 && (
+              <Descriptions title="Semana Actual" bordered>
+                <Descriptions.Item label="Pruebas en competencia " span={3}>
+                  {stats_personal[0].totalActivities}
+                </Descriptions.Item>
+                <Descriptions.Item label="Pruebas completadas" span={3}>
+                  {stats_personal[0].totalCompletedActivities}
+                </Descriptions.Item>
+                <Descriptions.Item label="Minutos ejercitados" span={3}>
+                  {stats_personal[0].totalDuration}
+                </Descriptions.Item>
+                <Descriptions.Item label="Kcal quemadas" span={3}>
+                  {stats_personal[0].totalCaloriesBurned}
+                </Descriptions.Item>
+              </Descriptions>
+            )}
           </Col>
-          <Col xl={11} xs={24} style={{marginTop:"-23px", }}>
-          {stats_personal && <>
-            <Title level={4} style={{marginBottom:"15px"}}>Registro competencía</Title>
-            <Table
-              bordered
-              style={{ marginBottom: "10px" }}
-              rowClassName={"backtable"}
-              size="small"
-              pagination={{ pageSize: 1 }}
-              columns={[
-                { title: "Semana", width:"30%",dataIndex: "week", render: (text) => <span>{text.replace("Week ", "Semana ")}</span> },
-                { render: (obj) => <Row>
-                  <Descriptions >
-                    <Descriptions.Item label="Pruebas en competencia " span={3}>{obj.totalActivities}</Descriptions.Item>
-                    <Descriptions.Item label="Pruebas completadas" span={3}>{obj.totalCompletedActivities}</Descriptions.Item>
-                    <Descriptions.Item label="Minutos ejercitados" span={3}>{obj.totalDuration}</Descriptions.Item>
-                    <Descriptions.Item label="Kcal quemadas" span={3}>{obj.totalCaloriesBurned}</Descriptions.Item>
-                  </Descriptions>
-                </Row>},
-              ]}
-              dataSource={stats_personal}
-            /></>}
+          <Col xl={11} xs={24} style={{ marginTop: "-23px" }}>
+            {stats_personal && (
+              <>
+                <Title level={4} style={{ marginBottom: "15px" }}>
+                  Registro competencía
+                </Title>
+                <Table
+                  bordered
+                  style={{ marginBottom: "10px" }}
+                  rowClassName={"backtable"}
+                  size="small"
+                  pagination={{ pageSize: 1 }}
+                  columns={[
+                    {
+                      title: "Semana",
+                      width: "30%",
+                      dataIndex: "week",
+                      render: (text) => (
+                        <span>{text.replace("Week ", "Semana ")}</span>
+                      ),
+                    },
+                    {
+                      render: (obj) => (
+                        <Row>
+                          <Descriptions>
+                            <Descriptions.Item
+                              label="Pruebas en competencia "
+                              span={3}
+                            >
+                              {obj.totalActivities}
+                            </Descriptions.Item>
+                            <Descriptions.Item
+                              label="Pruebas completadas"
+                              span={3}
+                            >
+                              {obj.totalCompletedActivities}
+                            </Descriptions.Item>
+                            <Descriptions.Item
+                              label="Minutos ejercitados"
+                              span={3}
+                            >
+                              {obj.totalDuration}
+                            </Descriptions.Item>
+                            <Descriptions.Item label="Kcal quemadas" span={3}>
+                              {obj.totalCaloriesBurned}
+                            </Descriptions.Item>
+                          </Descriptions>
+                        </Row>
+                      ),
+                    },
+                  ]}
+                  dataSource={stats_personal}
+                />
+              </>
+            )}
           </Col>
         </Row>
       </Col>
@@ -403,41 +468,50 @@ function getWeekNumber(date) {
       </Col>
       <Col span={24}>
         <Row justify={window.innerWidth > 900 ? "space-between" : "center"}>
-          {challengersUser.map((item) => (<Col>
-            <Card
-              hoverable
-              style={{ width: "250px", ...styles.card }}
-              cover={
-                <img
-                  alt="example"
-                  src={`http://localhost:8000/${item.activity.image}`}
-                />
-              }
-            >
-              <Card.Meta
-                title={
-                  <Title level={4} style={{ color: "white" }}>
-                    {item.activity.name}
-                  </Title>
+          {challengersUser.map((item) => (
+            <Col>
+              <Card
+                hoverable
+                style={{ width: "250px", ...styles.card }}
+                cover={
+                  <img
+                    alt="example"
+                    src={`http://localhost:8000/${item.activity.image}`}
+                  />
                 }
-                description={<>
-                <Paragraph level={4} style={{ color: "white" }}>
-                 {item.activity.description}</Paragraph>
-                  <Paragraph level={4} style={{ color: "white" }}>
-                   
-                    <ClockCircleFilled style={{ marginRight: "10px" }} />{" "}
-                    Inicio: {item.start_date_time.slice(0, 10)} <br/>
-                    <ClockCircleFilled style={{ marginRight: "10px" }} />{" "}
-                    Finalizar: {item.finish_date_time.slice(0, 10)}
-                  </Paragraph>
-                  </>}
-              />
-              <Button icon={<PlusCircleFilled />} style={{ float: "right" }} block>
-                Realizar
-              </Button>
-            </Card>
-          </Col>))}
-          
+              >
+                <Card.Meta
+                  title={
+                    <Title level={4} style={{ color: "white" }}>
+                      {item.activity.name}
+                    </Title>
+                  }
+                  description={
+                    <>
+                      <Paragraph level={4} style={{ color: "white" }}>
+                        {item.activity.description}
+                      </Paragraph>
+                      <Paragraph level={4} style={{ color: "white" }}>
+                        <ClockCircleFilled style={{ marginRight: "10px" }} />{" "}
+                        Inicio: {item.start_date_time.slice(0, 10)} <br />
+                        <ClockCircleFilled
+                          style={{ marginRight: "10px" }}
+                        />{" "}
+                        Finalizar: {item.finish_date_time.slice(0, 10)}
+                      </Paragraph>
+                    </>
+                  }
+                />
+                <Button
+                  icon={<PlusCircleFilled />}
+                  style={{ float: "right" }}
+                  block
+                >
+                  Realizar
+                </Button>
+              </Card>
+            </Col>
+          ))}
         </Row>
       </Col>
       <Col span={24} style={{ marginTop: "20px" }}>
@@ -462,24 +536,43 @@ function getWeekNumber(date) {
       </Col>
       <Col span={24}>
         <Row justify={"space-around"}>
-          {typeMeditions && typeMeditions.map((item) => 
-          <Col>
-          {stats_personal.map((stat) => {
-           return stat.totalCaloriesBurned > item.achievement_1  ? <div>
-           <CheckCircleFilled style={{color:"green", fontSize:"60px", padding:"10px"}} /><Text><center>10 ckal</center></Text></div>: 
-           <div><CheckCircleFilled style={{color:"grey", fontSize:"60px", padding:"10px"}} /></div>
-          
-          }
-            
-            )}
-          
-          {console.log(stats_personal)}
-            
-            <Title level={4} style={{textAlign:"center"}}>
-              {item.name.toUpperCase()}
-              </Title>
-          </Col>)}
-          
+          {typeMeditions &&
+            typeMeditions.map((item) => (
+              <Col>
+                {stats_personal.map((stat) => {
+                  return stat.totalCaloriesBurned > item.achievement_1 ? (
+                    <div>
+                      <CheckCircleFilled
+                        style={{
+                          color: "green",
+                          fontSize: "60px",
+                          padding: "10px",
+                        }}
+                      />
+                      <Text>
+                        <center>10 ckal</center>
+                      </Text>
+                    </div>
+                  ) : (
+                    <div>
+                      <CheckCircleFilled
+                        style={{
+                          color: "grey",
+                          fontSize: "60px",
+                          padding: "10px",
+                        }}
+                      />
+                    </div>
+                  );
+                })}
+
+                {console.log(stats_personal)}
+
+                <Title level={4} style={{ textAlign: "center" }}>
+                  {item.name.toUpperCase()}
+                </Title>
+              </Col>
+            ))}
         </Row>
       </Col>
       <Col span={24} style={{ marginTop: "20px" }}>
